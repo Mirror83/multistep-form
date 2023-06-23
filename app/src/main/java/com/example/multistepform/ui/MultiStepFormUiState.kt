b4cb78d99caebecab5a1b21b7fc75fb865eca717
Mirror83 <glenochieng045@gmail.com>
2023-06-23 15:16:17 +0300

    Added isFieldError property

--- app/src/main/java/com/example/multistepform/ui/MultiStepFormUiState.kt
+++ app/src/main/java/com/example/multistepform/ui/MultiStepFormUiState.kt
@@ -2,21 +2,32 @@ package com.example.multistepform.ui
 
 import com.example.multistepform.data.FormData
 
-data class PersonalInfo (var name: String, val email: String, val phoneNumber: String)
+data class PersonalInfo(
+    var name: String,
+    val email: String,
+    val phoneNumber: String,
+    val isNameError: Boolean = false,
+    val isPhoneError: Boolean = false,
+    val isEmailError: Boolean = false,
+)
 
 data class Plan(val name: String, val monthlyPrice: Int, val yearlyPrice: Int)
 
-data class AddOn(val name: String, val description: String, val monthlyPrice: Int, val yearlyPrice: Int)
+data class AddOn(
+    val name: String,
+    val description: String,
+    val monthlyPrice: Int,
+    val yearlyPrice: Int
+)
 
 enum class Period {
     Monthly, Yearly
 }
 
 
-data class MultiStepFormUiState (
+data class MultiStepFormUiState(
     val currentStep: Int = 1,
     val personalInfo: PersonalInfo = PersonalInfo("", "", ""),
-    val isFieldError: Boolean = false,
     val chosenAddOns: List<AddOn> = emptyList(),
     val plan: Plan = FormData.plans[0],
     val period: Period = Period.Monthly
