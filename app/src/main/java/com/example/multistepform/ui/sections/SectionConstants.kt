17b2bc91f96b05f13989f2b28cf44bd90f4d2852
Mirror83 <glenochieng045@gmail.com>
2023-07-25 12:53:27 +0300

    Added SectionConstants

---
+++ app/src/main/java/com/example/multistepform/ui/sections/SectionConstants.kt
@@ -0,0 +1,9 @@
+package com.example.multistepform.ui.sections
+
+object SectionConstants {
+    const val PERSONAL_INF0 = 1
+    const val PLAN = 2
+    const val ADD_ONS = 3
+    const val FINISHING_UP = 4
+    const val THANK_YOU = 5
+}
