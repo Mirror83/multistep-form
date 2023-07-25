460e38b53b8cc2dcb05b0d0c9c40ecb54d91ffb6
Mirror83 <glenochieng045@gmail.com>
2023-07-25 12:43:33 +0300

    Modified Plan to take an image resource ID as a parameter

--- app/src/main/java/com/example/multistepform/data/FormData.kt
+++ app/src/main/java/com/example/multistepform/data/FormData.kt
@@ -1,13 +1,14 @@
 package com.example.multistepform.data
 
+import com.example.multistepform.R
 import com.example.multistepform.ui.AddOn
 import com.example.multistepform.ui.Plan
 
 object FormData {
     val plans: List<Plan> = listOf(
-        Plan("Arcade", 9, 90),
-        Plan("Advanced", 12, 120),
-        Plan("Pro", 15, 150)
+        Plan(R.drawable.icon_arcade,"Arcade", 9, 90),
+        Plan(R.drawable.icon_advanced,"Advanced", 12, 120),
+        Plan(R.drawable.icon_pro,"Pro", 15, 150)
     )
 
     val addOns: List<AddOn> = listOf(
